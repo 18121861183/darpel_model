35d0bb9e02f66210ea2ea3cd19e53fbdd6a188e2
3195634799@qq.com <3195634799@qq.com>
2023-01-15 20:32:52 +0800

    Initial commit

---
+++ src/main/java/zhao/yao/chen/darpel/model/DarpelModelApplication.java
@@ -0,0 +1,13 @@
+package zhao.yao.chen.darpel.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarpelModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarpelModelApplication.class, args);
+    }
+
+}
